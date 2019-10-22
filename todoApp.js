443df64a9e7014b8b87ec7d03a13d67d1d796f72
John Hudson <johnhudson@John-Hudsons-MacBook-Air.local>
2019-10-22 13:02:19 -0400

    first push - beginning to build out todo app via udemy practice course.

---
+++ todoApp.js
@@ -0,0 +1,8 @@
+// Create an Array with five to dos
+// You have x todos
+// Print first and second to last
+
+const sprintOne = ['JavaScript Practice', 'React Practice', 'Update Resume', 'Update Portfolio', 'Job Applications']
+
+// Determines How Many index positions are in array
+console.log(sprintOne.length)
