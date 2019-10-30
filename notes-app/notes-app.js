69f083efa562928fb08b20f9255d14b50cf5a02d
John Hudson <johnhudson@John-Hudsons-MacBook-Air.local>
2019-10-30 15:25:25 -0400

    live filtering of notes and sprints app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,19 +1,3 @@
-const sprintOne = [{
-    text: 'JavaScript Practice',
-    completed: false
-}, {
-    text: 'React Practice',
-    completed: true
-}, {
-    text: 'Update Resume',
-    completed: true
-}, {
-    text: 'Update Portfolio',
-    completed: false
-}, {
-    text: 'Job Applications',
-    completed: true
-}]
 
 
 //  DOM - Document Object Model
@@ -25,24 +9,87 @@ const sprintOne = [{
 // console.log(p)
 
 // query all elements in the array and remove all p tags
-const ps = document.querySelectorAll('p')
-console.log(ps)
+// const ps = document.querySelectorAll('p')
+// console.log(ps)
 
 
-ps.forEach(function(p){
-    p.textContent ='******'
-    console.log(p.textContent)
+// ps.forEach(function(p){
+//     p.textContent ='******'
+//     console.log(p.textContent)
 
-})
+// })
 
 // Add a New Element
 
 // Step 1: Create new element using .createElement()
 
-const newParagraph = document.createElement('p')
+// const newParagraph = document.createElement('p')
 
 // Step 2: Use textContent method and add what you want to say.
-newParagraph.textContent = 'This is a new element from JavaScript.'
+// newParagraph.textContent = 'This is a new element from JavaScript.'
 
 // Step 3: .appendChild will add the element to the last item in the selector, body
-document.querySelector('body').appendChild(newParagraph)
+// document.querySelector('body').appendChild(newParagraph)
+
+const notes = [{
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+
+}, {
+    title: 'Habits to work on',
+    body: 'Exercise more and eat better'
+}, {
+    title: 'Office modification',
+    body: 'New chairs and new desk'
+}]
+const filters = {
+    searchText:''
+}
+
+// The function below is called to filter the notes and render them to the screen
+    // it is a function that takes in array of objects(notes) and filters (object)
+
+const renderNotes = function(notes, filters){
+
+    // STEP 1: filers the notes array and stores the returned values into the filteredNotes variable
+        //  returns the title in the element to lowercase that INCLUDES the words that have been typed into the input(filters.searchText.toLowercase string)
+    const filteredNotes = notes.filter(function(note){
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    // STEP 2: Clears everything in the attached element (#notes DIV via HTML)
+        // .innerHTML sets everything to an empty string 
+    document.querySelector('#notes').innerHTML = ''
+
+    // STEP 3: Loops through the filteredNotes /creating a new element in #notes NotesDIV
+    filteredNotes.forEach(function(note){
+        // create Note element
+        const noteEl = document.createElement('p')
+        noteEl.textContent = note.title
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+
+// Create Note Button
+document.querySelector('#createNote').addEventListener('click',function(){
+    console.log('CREATED Note')
+   
+})
+
+// Remove Note Button
+document.querySelector('#removeNote').addEventListener('click', function(){
+    
+    document.querySelectorAll('.note').forEach(function(note){
+        note.remove() 
+    })
+})
+
+// input field with id="searchText"
+
+document.querySelector('#searchText').addEventListener('input', function(e){
+    // search text on the filters object gets set = to what is in the input field (e.target.value)
+    filters.searchText = e.target.value
+    // function runs everytime input is changed / something is input in box renders everytime input changes
+    renderNotes(notes, filters)
+})
--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,24 +15,40 @@ const sprintOne = [{
     completed: true
 }]
 
+// 1. Setup new div to contain the new sprints
+// 2. Setup filters object with searchtext propety and wire up a new filter input to change it
+// 3. Create a renderSprints function to render and re-render the latest filtered data
+
+
 // This filters the sprintOne array and places the sprints that are false in a new array called incompleteSprints
 
+
+// Starts
 const incompleteSprints = sprintOne.filter(function (sprint) {
     return !sprint.completed
 })
-
 // Below is where we created an H2 tag and used the incompleteSprints Array to show in DOM how many tasks are remaining (marked false)
 const summary = document.createElement('h2');
 summary.textContent = `You have ${incompleteSprints.length} sprints remaining.`
 document.querySelector('body').appendChild(summary)
+// ends
+
+
+
 
 // We looped through the sprintOne array using forEach and printed them to the DOM
 // createElement = use variable with element you want to create as argument
-// 
-
 sprintOne.forEach(function(sprint){
     const p = document.createElement('p')
     p.textContent = sprint.text
     document.querySelector('body').appendChild(p)
 
+})
+
+document.querySelector('#createSprint').addEventListener('click', function(e){
+    console.log("New Spirnt Created")
+})
+
+document.querySelector('#inputSprint').addEventListener('input', function(e){
+    console.log(e.target.value)
 })
