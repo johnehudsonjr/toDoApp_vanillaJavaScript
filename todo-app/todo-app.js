0bc88a6e62362abf65b0039a7c91fbfe3de18927
John Hudson <johnhudson@John-Hudsons-MacBook-Air.local>
2019-10-30 16:39:42 -0400

    Updated app with notes as well as created function to show rendering and update the remainign sprints number.

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -15,40 +15,68 @@ const sprintOne = [{
     completed: true
 }]
 
-// 1. Setup new div to contain the new sprints
-// 2. Setup filters object with searchtext propety and wire up a new filter input to change it
-// 3. Create a renderSprints function to render and re-render the latest filtered data
+// Created a new filters object, with empty searchText string where our searched content will be placed
+const filters = {
+    searchText: ''
+}
+
+// Created a function that takes in the sprint array and the filters object
+const renderSprints = function (sprintOne, filters) {
+
+    // Filtered the sprints and RETURNED the new filtered array to filteredSprints
+            // return the text of sprint (sprint.text) lower case that INCLUDES what is present in the filters|searchText property(lowercase)
+    const filteredSprints = sprintOne.filter(function (sprint) {
+        return sprint.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+    // This line clears the #sprints (id) Div by setting the innerHTML content to a string.
+     document.querySelector('#sprints').innerHTML = ''
+
+    // This line of code filters through the filteredSprints and returns the sprints that are false !sprint.completed
+    const incompleteSprints = filteredSprints.filter(function (sprint) {
+        return !sprint.completed
+    })
+
+    // Here is where I created an  H2 tag and used the incompleteSprints Array to show in DOM how many tasks are remaining (marked false)
+        // Create element
+    const summary = document.createElement('h2');
+        // Change text content using template string
+    summary.textContent = `You have ${incompleteSprints.length} sprints remaining.`
+        // append the created element to the selected node
+    document.querySelector('#sprints').appendChild(summary)
+  
+    // This code will loop through the filteredSprints array and create a new p tag for each element that is being filtered based on condition from filteredSprints
+    filteredSprints.forEach(function (sprint) {
+        const sprintEl = document.createElement('p')
+        sprintEl.textContent = sprint.text
+        document.querySelector('#sprints').appendChild(sprintEl)
+    })
 
 
-// This filters the sprintOne array and places the sprints that are false in a new array called incompleteSprints
+}
 
-
-// Starts
-const incompleteSprints = sprintOne.filter(function (sprint) {
-    return !sprint.completed
-})
-// Below is where we created an H2 tag and used the incompleteSprints Array to show in DOM how many tasks are remaining (marked false)
-const summary = document.createElement('h2');
-summary.textContent = `You have ${incompleteSprints.length} sprints remaining.`
-document.querySelector('body').appendChild(summary)
-// ends
-
-
+// renderSprints function call with sprintOne -array and filters object
+renderSprints(sprintOne, filters)
 
 
 // We looped through the sprintOne array using forEach and printed them to the DOM
 // createElement = use variable with element you want to create as argument
-sprintOne.forEach(function(sprint){
-    const p = document.createElement('p')
-    p.textContent = sprint.text
-    document.querySelector('body').appendChild(p)
+// sprintOne.forEach(function (sprint) {
+//     const p = document.createElement('p')
+//     p.textContent = sprint.text
+//     document.querySelector('body').appendChild(p)
 
-})
+// })
 
-document.querySelector('#createSprint').addEventListener('click', function(e){
+document.querySelector('#createSprint').addEventListener('click', function (e) {
     console.log("New Spirnt Created")
 })
 
-document.querySelector('#inputSprint').addEventListener('input', function(e){
-    console.log(e.target.value)
+
+// Input Search Field
+    // This takes what is  typed into the input field (eventListener) and set what's typed(e.target.value) equal to the filters object (filters.searchText) 
+    // It calls renderSprints, running the function everytime something is typed jn input field
+document.querySelector('#inputSprint').addEventListener('input', function (e) {
+    filters.searchText = e.target.value
+    renderSprints(sprintOne, filters)
+
 })
